f95c23845bbaff7744d6284c5e6512113436b7d3
Aleksandr Kazirickiy <kozirfm@yandex.ru>
2020-11-15 22:52:11 +0300

    HomeWork, lesson3

--- app/src/main/java/ru/kozirfm/materialcourse/MainActivity.kt
+++ app/src/main/java/ru/kozirfm/materialcourse/MainActivity.kt
@@ -1,11 +1,27 @@
 package ru.kozirfm.materialcourse
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            showDialog().show()
+        }
+
+    }
+
+    private fun showDialog(): Dialog {
+        return MaterialAlertDialogBuilder(this, R.style.DialogStyle)
+            .setTitle(resources.getString(R.string.dialog))
+            .setMessage(resources.getString(R.string.dialog_message))
+            .setPositiveButton("OK") { dialogInterface, _ ->
+                dialogInterface.dismiss()
+            }.create()
     }
 }
